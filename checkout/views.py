e5d5065f08cabdb2f6faa7aea09664aaaaa9bac3
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-12-05 00:17:51 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse 
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
-from django.conf import settings 
+from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
-import stripe
-import json 
+import stripe 
+import json
 
 @require_POST
 def cache_checkout_data(request):
@@ -25,7 +25,8 @@ def cache_checkout_data(request):
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
-        return HttpResponse(content=e, status=400) 
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -47,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -108,7 +113,8 @@ def checkout(request):
         'client_secret': intent.client_secret,
     }
 
-    return render(request, template, context) 
+    return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
