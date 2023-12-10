9f685b78c2dd985c2d5ce64d5213626512c22330
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-12-10 17:11:04 +0000

    Database urls

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
  
 from pathlib import Path
 import os 
+import dj_database_url 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +126,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
 
 
 # Password validation
