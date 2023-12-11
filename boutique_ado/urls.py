aa33ab27203cdaa4360b9c22ad8e4cc3d42fe7ac
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-12-11 22:51:05 +0000

    Add error pages and error functions

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+handler404 = 'boutique_ado.views.handler404'
+handler500 = 'boutique_ado.views.handler500'
